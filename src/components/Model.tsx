00e98f9d55ecb745b37078e928a890bab145b2ee
admin <admin@MacBookPro.lan>
2025-11-22 17:55:12 +0530

    add buttons to the model

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,13 +1,21 @@
-import { Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function Model (){
+    const [newItem,setNewItem] = useState("");
+    const [newItemPrice,setNewItemPrice] = useState("");
     return (
         <View>
             <Text>Add</Text>
 
             <View>
-                <TextInput placeholder="Item" />
-                <TextInput placeholder="Price" />
+                <TextInput placeholder="Item" value={newItem} onChangeText={setNewItem}/>
+                <TextInput placeholder="Price" value={newItemPrice} onChangeText={setNewItemPrice}/>
+            </View>
+
+            <View>
+                <Button title="Add"/>
+                <Button title="Cancel" />
             </View>
         </View>
     )

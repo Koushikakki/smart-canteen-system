726b732713868335a743e3975ed4b60b2bc99edd
admin <admin@MacBookPro.lan>
2025-11-22 16:17:58 +0530

    add types for the items and sections

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+import { ImageSourcePropType } from "react-native"
+
+export type Item = {
+    id : string,
+    title : string,
+    price : number,
+    image : ImageSourcePropType
+};
+
+export type Section = {
+    id : string,
+    title : string,
+    data : Item[];
+};

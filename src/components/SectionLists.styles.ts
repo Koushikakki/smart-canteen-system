774b176b48bae6923b14ffdc3710b02665919610
admin <admin@MacBookPro.lan>
2025-11-22 16:23:12 +0530

    add styles for the section list component

---
+++ src/components/SectionLists.styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+    sectionTitle : {
+        color : "#1b6edb",
+        fontSize: 22,
+        fontWeight:'600',
+        marginHorizontal : 16,
+        marginTop: 20,
+        marginBottom :8
+    },
+    sectionHeader : {
+        backgroundColor : "#f5f7fb",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    }
+})

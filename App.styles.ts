0da30313b586f789b4087e04a442d398a76279e1
admin <admin@MacBookPro.lan>
2025-11-22 16:24:30 +0530

    add styles for the app

---
+++ App.styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native"
+export default StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingBottom :70
+    },
+    heading : {
+      textAlign:'center',
+      fontSize : 28,
+      fontWeight : '700',
+      color: "#000001ff",
+      marginHorizontal : 16,
+      marginBottom : 20
+    }
+})

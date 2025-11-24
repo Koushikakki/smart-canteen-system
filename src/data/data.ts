d4bb1ef36de2d64c425a70d45b95799e80099370
admin <admin@MacBookPro.lan>
2025-11-24 10:46:04 +0530

    commented the image url

--- src/data/data.ts
+++ src/data/data.ts
@@ -7,23 +7,23 @@ export const menuData: Section[] = [
         data: [
             {
                 id: 'b1', title: 'Coffee', price: 50,
-                image: require('../assets/images/beverages/coffee.jpeg')
+                // image: require('../assets/images/beverages/coffee.jpeg')
             },
             {
                 id: 'b2', title: 'Tea', price: 30,
-                image: require('../assets/images/beverages/tea.jpeg')
+                // image: require('../assets/images/beverages/tea.jpeg')
             },
             {
                 id: 'b3', title: 'Hot Chocolate', price: 50,
-                image: require('../assets/images/beverages/hot-chocolate.jpeg')
+                // image: require('../assets/images/beverages/hot-chocolate.jpeg')
             },
             {
                 id: 'b4', title: 'Lassi', price: 30,
-                image: require('../assets/images/beverages/lassi.jpeg')
+                // image: require('../assets/images/beverages/lassi.jpeg')
             },
             {
                 id: 'b5', title: 'Butter Milk', price: 20,
-                image: require('../assets/images/beverages/buttermilk.jpeg')
+                // image: require('../assets/images/beverages/buttermilk.jpeg')
             }
         ]
     },
@@ -33,23 +33,23 @@ export const menuData: Section[] = [
         data: [
             {
                 id: 'b1', title: 'Idly', price: 30,
-                image: require('../assets/images/breakfast/idly.jpeg')
+                // image: require('../assets/images/breakfast/idly.jpeg')
             },
             {
                 id: 'b2', title: 'Dosa', price: 40,
-                image: require('../assets/images/breakfast/dosa.jpeg')
+                // image: require('../assets/images/breakfast/dosa.jpeg')
             },
             {
                 id: 'b3', title: 'Poori', price: 30,
-                image: require('../assets/images/breakfast/poori.jpeg')
+                // image: require('../assets/images/breakfast/poori.jpeg')
             },
             {
                 id: 'b4', title: 'Upma', price: 30,
-                image: require('../assets/images/breakfast/upma.jpeg')
+                // image: require('../assets/images/breakfast/upma.jpeg')
             },
             {
                 id: 'b5', title: 'Omelette', price: 40,
-                image: require('../assets/images/breakfast/omelette.jpeg')
+                // image: require('../assets/images/breakfast/omelette.jpeg')
             }
         ]
     },
@@ -59,23 +59,23 @@ export const menuData: Section[] = [
         data: [
             {
                 id: 'm1', title: 'Veg Thali', price: 80,
-                image: require('../assets/images/mainCourse/veg-thali.jpeg')
+                // image: require('../assets/images/mainCourse/veg-thali.jpeg')
             },
             {
                 id: 'm2', title: 'Chicken Biryani', price: 150,
-                image: require('../assets/images/mainCourse/chicken-biryani.jpeg')
+                // image: require('../assets/images/mainCourse/chicken-biryani.jpeg')
             },
             {
                 id: 'm3', title: 'Veg Biryani', price: 100,
-                image: require('../assets/images/mainCourse/veg-biryani.jpeg')
+                // image: require('../assets/images/mainCourse/veg-biryani.jpeg')
             },
             {
                 id: 'm4', title: 'Curd Rice', price: 40,
-                image: require('../assets/images/mainCourse/curd-rice.jpeg')
+                // image: require('../assets/images/mainCourse/curd-rice.jpeg')
             },
             {
                 id: 'm5', title: 'Chicken Curry', price: 120,
-                image: require('../assets/images/mainCourse/chicken-curry.jpeg')
+                // image: require('../assets/images/mainCourse/chicken-curry.jpeg')
             }
         ]
     },
@@ -85,23 +85,23 @@ export const menuData: Section[] = [
         data: [
             {
                 id: 'd1', title: 'Vanilla IceCream', price: 30,
-                image: require('../assets/images/desserts/vanilla-icecream.jpeg')
+                // image: require('../assets/images/desserts/vanilla-icecream.jpeg')
             },
             {
                 id: 'd2', title: 'Strawberry IceCream', price: 40,
-                image: require('../assets/images/desserts/strawberry-icecream.jpeg')
+                // image: require('../assets/images/desserts/strawberry-icecream.jpeg')
             },
             {
                 id: 'd3', title: 'Gulab Jamun', price: 20,
-                image: require('../assets/images/desserts/gulab-jamun.jpeg')
+                // image: require('../assets/images/desserts/gulab-jamun.jpeg')
             },
             {
                 id: 'd4', title: 'Kheer', price: 30,
-                image: require('../assets/images/desserts/kheer.jpeg')
+                // image: require('../assets/images/desserts/kheer.jpeg')
             },
             {
                 id: 'd5', title: 'Rasgulla', price: 30,
-                image: require('../assets/images/desserts/rasagulla.jpeg') // fixed spelling
+                // image: require('../assets/images/desserts/rasagulla.jpeg') 
             }
         ]
     },
@@ -111,23 +111,23 @@ export const menuData: Section[] = [
         data: [
             {
                 id: 's1', title: 'Samosa', price: 30,
-                image: require('../assets/images/snacks/samosa.jpeg')
+                // image: require('../assets/images/snacks/samosa.jpeg')
             },
             {
                 id: 's2', title: 'French Fries', price: 50,
-                image: require('../assets/images/snacks/french-fries.jpeg')
+                // image: require('../assets/images/snacks/french-fries.jpeg')
             },
             {
                 id: 's3', title: 'Sandwich', price: 40,
-                image: require('../assets/images/snacks/sandwich.jpeg')
+                // image: require('../assets/images/snacks/sandwich.jpeg')
             },
             {
                 id: 's4', title: 'Pizza', price: 120,
-                image: require('../assets/images/snacks/pizza.jpeg')
+                // image: require('../assets/images/snacks/pizza.jpeg')
             },
             {
                 id: 's5', title: 'Burger', price: 80,
-                image: require('../assets/images/snacks/burger.jpeg')
+                // image: require('../assets/images/snacks/burger.jpeg')
             }
         ]
     }

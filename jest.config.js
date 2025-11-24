641b7de51fad132bac72235ab2dd65d748700d71
admin <admin@MacBookPro.lan>
2025-11-24 16:18:44 +0530

    update the jest config file to test the app

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  testEnvironment: "react-native",
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.styles.ts",
+    "!src/**/index.ts",
+    "App.tsx"
+  ]
 };

53e2e7afe3f65e075634ba0e73f5929ef562e612
admin <admin@MacBookPro.lan>
2025-11-24 10:47:17 +0530

    add sections state to add or delete data

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar, useColorScheme, View ,Text} from 'react-native';
+import { View ,Text} from 'react-native';
 import {
   SafeAreaProvider,
 } from 'react-native-safe-area-context';
 import SectionLists from './src/components/SectionLists';
 import { menuData } from './src/data/data';
 import styles from './App.styles'
+import { useState } from 'react';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+
+  const [sections,setSections]=useState(menuData);
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      
       <View style={styles.container}>
       <View><Text style={styles.heading}>Everest Canteen</Text></View>
-      <SectionLists sections={menuData}/>
+      <SectionLists sections={sections}
+      setSections={setSections}/>
     </View>
     </SafeAreaProvider>
   );

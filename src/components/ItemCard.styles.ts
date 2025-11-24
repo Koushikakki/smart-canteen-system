aad7cbe3f478429fb428f81291c90830e596e40d
admin <admin@MacBookPro.lan>
2025-11-24 10:50:56 +0530

    add styles for the black box inplace of image for now

--- src/components/ItemCard.styles.ts
+++ src/components/ItemCard.styles.ts
@@ -1,43 +1,46 @@
-import { Dimensions , StyleSheet} from "react-native";
-const {width} = Dimensions.get('window');
+import { Dimensions, StyleSheet } from 'react-native';
+const { width } = Dimensions.get('window');
 export default StyleSheet.create({
-    card : {
-        width : width-32,
-        height  : 130,
-        borderRadius : 15,
-        backgroundColor : '#f7f5f5',
-        marginHorizontal : 16,
-        marginVertical: 8,
-        flexDirection : "row",
-        alignItems : 'center'
+  card: {
+    width: width - 32,
+    height: 130,
+    borderRadius: 15,
+    backgroundColor: '#f7f5f5',
+    marginHorizontal: 16,
+    marginVertical: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 70,
+    height: 70,
+    backgroundColor: '#000',
+    borderRadius: 6,
+    marginRight: 12,
+  },
 
-    },
+  cardImage: {
+    width: '50%',
+    height: '80%',
+    borderRadius: 15,
+    marginRight: 16,
+  },
 
-    cardImage : {
-        width : '50%',
-        height : '80%',
-        borderRadius:15,
-        marginRight: 16,
-    },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
 
-    textContainer: {
-        flex: 1,
-        justifyContent: "center",
-        
-    },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 6,
+    color: '#333',
+  },
 
-    title: {
-        fontSize: 18,
-        fontWeight: "600",
-        marginBottom: 6,
-        color: "#333",
-    },
-
-    price: {
-        fontSize: 16,
-        color: "#1b6edb",
-        fontWeight: "bold",
-        
-    },
-
-})
+  price: {
+    fontSize: 16,
+    color: '#1b6edb',
+    fontWeight: 'bold',
+  },
+});

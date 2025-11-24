c3ab36bbecc25cde0c396a4461b209a919110146
admin <admin@MacBookPro.lan>
2025-11-24 10:46:26 +0530

    commit image type for now

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,10 @@
-import { ImageSourcePropType } from "react-native"
+// import { ImageSourcePropType } from "react-native"
 
 export type Item = {
     id : string,
     title : string,
     price : number,
-    image : ImageSourcePropType
+    // image : ImageSourcePropType
 };
 
 export type Section = {

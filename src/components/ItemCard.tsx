d369a37beaa58171b1286b755330b1fc350fbaee
admin <admin@MacBookPro.lan>
2025-11-24 10:50:08 +0530

    commented the image for now and add black box in the ui

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,11 +1,14 @@
-import { Image,Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Item } from "../types/types";
 import styles from "./ItemCard.styles.ts"
 
 export function ItemCard ({item} : {item : Item}) {
     return(
         <View style={styles.card}>
-            <Image source={item.image} style ={styles.cardImage} />
+            {/* <Image source={item.image} style ={styles.cardImage} /> */}
+            <View style={styles.image} />
+
+            
             <View style={styles.textContainer}>
                 <Text style={styles.title}>{item.title}</Text>
                 <Text style={styles.price}>{item.price}</Text>

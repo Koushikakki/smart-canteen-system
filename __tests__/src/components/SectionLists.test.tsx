a047c63f0a3b37239fd1e1240f316f4ba2c6ee85
admin <admin@MacBookPro.lan>
2025-11-25 18:57:47 +0530

    moved the mocked section data into the dexcribe

--- __tests__/src/components/SectionLists.test.tsx
+++ __tests__/src/components/SectionLists.test.tsx
@@ -1,27 +1,23 @@
-import { render } from "@testing-library/react-native";
-import SectionLists from "../../../src/components/SectionLists";
+import { render } from '@testing-library/react-native';
+import SectionLists from '../../../src/components/SectionLists';
 
-const mockSections = [
-  {
-    id : 's1',
-    title: "Main Course",
-    data: [
-      { id: "1", title: "Pizza", price: 10, image: require("/Users/admin/Desktop/react-native/canteen-management-system/assets/images/snacks/pizza.jpeg") }
-    ],
-  },
-];
+const mockSetSections = jest.fn();
 
-describe("SectionList component",()=>{
-    test("sections are rendered or not",()=>{
-        const {getByText} = render(<SectionLists sections={mockSections}/>);
-        expect(getByText("Main Course")).toBeOnTheScreen();
-        expect(getByText("Pizza")).toBeOnTheScreen();
-        
-    });
+describe('SectionList component', () => {
+  const mockSections = [
+    {
+      id: 's1',
+      title: 'Main Course',
+      data: [{ id: '1', title: 'Pizza', price: 10 }],
+    },
+  ];
+  test('sections are rendered or not', () => {
+    const { getByText } = render(
+      <SectionLists sections={mockSections} setSections={mockSetSections} />,
+    );
+    expect(getByText(/Main Course/i)).toBeTruthy();
+    expect(getByText(/Pizza/i)).toBeTruthy();
+  });
 
-    test("image is rendering or not",()=>{
-      const {getByTestId} = render(<SectionLists sections={mockSections}/>);
-      const image = getByTestId("item-image");
-      expect(image.props.source).toBe(mockSections[0].data[0].image);
-    })
-})
+  
+});

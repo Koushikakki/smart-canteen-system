3998df27936d98966d86e6cd56b9059b871de3aa
admin <admin@MacBookPro.lan>
2025-11-26 18:19:55 +0530

    add safeareaprovider for the App

--- App.tsx
+++ App.tsx
@@ -1,25 +1,21 @@
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import SectionLists from './src/components/SectionLists';
 import { menuData } from './src/data/data';
 import styles from './App.styles';
 import { useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
   const [sections, setSections] = useState(menuData);
 
   return (
-
-    <SafeAreaView style={styles.container}>
-        
-      <View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.heading}>Everest Canteen</Text>
-      </View>
-      <SectionLists sections={sections} setSections={setSections} />
 
-    </SafeAreaView>
-
-    
+        <SectionLists sections={sections} setSections={setSections} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 export default App;

9b066ec1ca614d3c22db5e1762127724de03bb1a
admin <admin@MacBookPro.lan>
2025-11-28 16:54:25 +0530

    add navigations to the sign in page

--- src/screens/loginScreen/LoginScreen.tsx
+++ src/screens/loginScreen/LoginScreen.tsx
@@ -1,15 +1,15 @@
 import { Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './LoginScreen.styles';
 
-export default function LoginScreen() {
+export default function LoginScreen({navigation} : {navigation:any}) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Everest Canteen</Text>
       <View style={styles.buttons}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate('SignIn')}>
           <Text style={styles.buttonText}>LOGIN AS USER</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate('SignIn')}>
           <Text style={styles.buttonText}>LOGIN AS ADMIN</Text>
         </TouchableOpacity>
       </View>

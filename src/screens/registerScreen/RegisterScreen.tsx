e81e27516e3bd52fe0394254f4b542922481bf20
admin <admin@MacBookPro.lan>
2025-11-28 15:58:00 +0530

    add the register screen to register a new user

---
+++ src/screens/registerScreen/RegisterScreen.tsx
@@ -0,0 +1,36 @@
+import { Text, View, TouchableOpacity ,TextInput} from 'react-native';
+import { styles } from './RegisterScreen.styles';
+export function RegisterScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Create your account</Text>
+      <View style={styles.inputField}>
+        <Text style={styles.inputLabel}>Email </Text>
+        <TextInput placeholder="Enter your email" style={styles.textInput} />
+        <Text style={styles.inputLabel}>Password</Text>
+        <TextInput
+          placeholder="Password"
+          style={styles.textInput}
+          secureTextEntry
+        />
+        <Text style={styles.inputLabel}>Confirm Password</Text>
+        <TextInput
+          placeholder="Confirm Password"
+          style={styles.textInput}
+          secureTextEntry
+        />
+
+      </View>
+
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>SIGN IN</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.textPrompt}>Have an account?</Text>
+            <TouchableOpacity style={styles.signInButton}>
+              <Text style={styles.signInText}>Sign In</Text>
+            </TouchableOpacity>
+    </View>
+  );
+}

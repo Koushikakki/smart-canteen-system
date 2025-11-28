2bc9ee77cc3665ed0dd55b350037b978733f1f61
admin <admin@MacBookPro.lan>
2025-11-28 18:54:48 +0530

    add role to the navigation to differntiate user and admin login

--- src/screens/loginScreen/LoginScreen.tsx
+++ src/screens/loginScreen/LoginScreen.tsx
@@ -1,15 +1,21 @@
 import { Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './LoginScreen.styles';
 
-export default function LoginScreen({navigation} : {navigation:any}) {
+export default function LoginScreen({ navigation}: { navigation: any }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Everest Canteen</Text>
       <View style={styles.buttons}>
-        <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate('SignIn')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('SignIn',{role : 'user'})}
+        >
           <Text style={styles.buttonText}>LOGIN AS USER</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate('SignIn')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('SignIn',{role : 'admin'})}
+        >
           <Text style={styles.buttonText}>LOGIN AS ADMIN</Text>
         </TouchableOpacity>
       </View>

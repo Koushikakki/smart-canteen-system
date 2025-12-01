6a43faad6c3b7b4cfff3b9db41c2273996045c02
admin <admin@MacBookPro.lan>
2025-12-01 11:06:27 +0530

    add styles for the listing the elements in the userHome

---
+++ src/components/UserItemList.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  listContainer: {
+    paddingVertical: 8,
+  },
+  sectionHeader: {
+    backgroundColor: '#f0f0f0',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#333',
+    textTransform: 'capitalize',
+  },
+});

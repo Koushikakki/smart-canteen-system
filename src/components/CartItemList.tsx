72a44b600327c345716662b794550acc096defb0
admin <admin@admins-MacBook-Pro.local>
2025-12-01 13:22:11 +0530

    add the quantity to the list

--- src/components/CartItemList.tsx
+++ src/components/CartItemList.tsx
@@ -3,11 +3,17 @@ import { FlatList } from 'react-native';
 import { CartItem } from '../types/types';
 import { CartItemCard } from './CartItemCard';
 
-export function CartItemList({ cartItems }:{cartItems :CartItem[]}){
+type props ={
+    cartItems : CartItem[];
+    onIncrease :(id : string)=> void;
+    onDecrease : (id :string)=> void;
+}
+
+export function CartItemList({ cartItems ,onDecrease,onIncrease}:props){
   return (
     <FlatList
       data={cartItems}
-      renderItem={({ item }) => <CartItemCard item={item} />}
+      renderItem={({ item }) => <CartItemCard item={item} onDecrease={onDecrease} onIncrease={onIncrease}/>}
       keyExtractor={item => item.id}
     />
   );

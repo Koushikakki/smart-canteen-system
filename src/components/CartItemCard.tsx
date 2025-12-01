bda5928b4a5adcd7c9c3d3357106a385117d1de6
admin <admin@admins-MacBook-Pro.local>
2025-12-01 13:21:40 +0530

    add quantity for the itemcard

--- src/components/CartItemCard.tsx
+++ src/components/CartItemCard.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,TouchableOpacity } from 'react-native';
 import { CartItem } from '../types/types';
 import { styles } from './UserCartStyles.styles';
 
-export function CartItemCard ({ item } : {item : CartItem})  {
+type props ={
+    item : CartItem;
+    onIncrease :(id : string)=> void;
+    onDecrease : (id :string)=> void;
+}
+
+
+export function CartItemCard ({ item ,onIncrease,onDecrease} : props)  {
   return (
     <View style={styles.cartItem}>
       <Text style={styles.itemTitle}>{item.title}</Text>
+      <View style={styles.quantityContainer}>
+        <TouchableOpacity onPress={() => onDecrease(item.id)} style={styles.quantityButton}>
+          <Text style={styles.quantityButtonText}>-</Text>
+        </TouchableOpacity>
+        <Text style={styles.quantityText}>{item.quantity}</Text>
+        <TouchableOpacity onPress={() => onIncrease(item.id)} style={styles.quantityButton}>
+          <Text style={styles.quantityButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
       <Text style={styles.itemPrice}>{item.price}</Text>
     </View>
   );

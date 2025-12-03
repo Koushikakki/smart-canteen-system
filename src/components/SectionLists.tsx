1586d89145eb9e7e38220caa07bff3760be430dc
admin <admin@MacBookPro.lan>
2025-12-03 16:42:56 +0530

    add logic to display add new item button for the admin but not user

--- src/components/SectionLists.tsx
+++ src/components/SectionLists.tsx
@@ -8,9 +8,16 @@ import Model from './Model.tsx';
 type props = {
   sections: Section[];
   setSections: (value: Section[]) => void;
+  addToCart?: (item: any) => void;
+  isAdmin?: boolean;
 };
 
-export function SectionLists({ sections, setSections }: props) {
+export function SectionLists({
+  sections,
+  setSections,
+  addToCart,
+  isAdmin,
+}: props) {
   const [model, setModel] = useState(false);
   const [selectedSection, setSelectedSection] = useState<Section | null>(null);
 
@@ -29,27 +36,36 @@ export function SectionLists({ sections, setSections }: props) {
         sections={sections}
         keyExtractor={item => item.id}
         stickySectionHeadersEnabled={true}
-        renderItem={({ item }) => <ItemCard item={item} />}
+        renderItem={({ item }) => (
+          <ItemCard item={item} onAdd={() => addToCart && addToCart(item)   } isAdmin={isAdmin} />
+        )}
         renderSectionHeader={({ section }) => (
           <View style={styles.sectionHeader}>
             <Text style={styles.sectionTitle}>
               {section.title} {section.data.length}
             </Text>
-            <TouchableOpacity
-              style={styles.addButton}
-              testID='add'
-              onPress={() => {
-                setSelectedSection(section);
-                openModel(section);
-              }}
-            >
-              <Text style={styles.addButtonText}>Add</Text>
-            </TouchableOpacity>
+            {isAdmin && (
+              <TouchableOpacity
+                style={styles.addButton}
+                testID="add"
+                onPress={() => {
+                  setSelectedSection(section);
+                  openModel(section);
+                }}
+              >
+                <Text style={styles.addButtonText}>Add</Text>
+              </TouchableOpacity>
+            )}
           </View>
         )}
       />
 
-      <Modal visible={model} transparent={true} animationType="slide" testID='add-model'>
+      <Modal
+        visible={model}
+        transparent={true}
+        animationType="slide"
+        testID="add-model"
+      >
         {model && selectedSection && (
           <Model
             onclose={closeModel}

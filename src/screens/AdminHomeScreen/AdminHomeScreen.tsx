8858039effdf7fb49e480860f79b6885ff9a9721
admin <admin@MacBookPro.lan>
2025-12-03 16:44:50 +0530

    add isAdmin prop to display the admin contents in the app

--- src/screens/AdminHomeScreen/AdminHomeScreen.tsx
+++ src/screens/AdminHomeScreen/AdminHomeScreen.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+
 import SectionLists from '../../components/SectionLists';
 import { Text} from 'react-native';
 import { SafeAreaView,SafeAreaProvider } from 'react-native-safe-area-context';
 import styles from './AdminHomeScreen.styles'
+import { Section } from '../../types/types';
+import { useState } from 'react';
 import { menuData } from '../../data/data';
-export default function AdminHomeScreen() {
-  const [sections, setSections] = useState(menuData);
 
+export default function AdminHomeScreen() {
+  const [sections, setSections] = useState<Section[]>(menuData);
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <Text style={styles.heading}>Everest Canteen</Text>
 
-        <SectionLists sections={sections} setSections={setSections} />
+        <SectionLists sections={sections} setSections={setSections} isAdmin={true} />
       </SafeAreaView>
     </SafeAreaProvider>
   );

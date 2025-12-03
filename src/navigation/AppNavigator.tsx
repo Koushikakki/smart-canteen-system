a767cf86ffd5a88735bf32ea37ea068cbd0e1fe2
admin <admin@MacBookPro.lan>
2025-12-03 16:30:41 +0530

    add the naviagtion to the User Screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,24 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import LoginScreen from '../screens/loginScreen/LoginScreen';
 import SignInScreen from '../screens/signInScreen/SignInScreen';
 import { RegisterScreen } from '../screens/registerScreen/RegisterScreen';
 import AdminHomeScreen from '../screens/AdminHomeScreen/AdminHomeScreen';
+import UserScreen from '../screens/UserScreen/UserScreen';
 
 const Stack = createNativeStackNavigator();
 
 export function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="AdminHomeScreen" component={AdminHomeScreen}/>
+        <Stack.Screen name="AdminHomeScreen" component={AdminHomeScreen} />
+        <Stack.Screen name="UserHomeScreen" component={UserScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
